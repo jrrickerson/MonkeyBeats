7cbf85d410a0cf3c06efc0561845487ce6ff7690
JR Rickerson <jrrickerson@gmail.com>
2010-01-19 02:54:49 +0800

    Added skeleton for unit tests on file filters

---
+++ tests/test_fileFilters.py
@@ -0,0 +1,23 @@
+import unittest
+import nose
+
+import fileFilters
+
+# Fixture setup and teardown
+def setUpModule():
+    pass
+def tearDownModule():
+    pass
+
+class Mp3FileFilterTestFixture(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def testAllSourceFilesMatchFilter(self):
+        self.fail("Not yet implemented") 
+
+    def testNoSourceFilesMatchFilter(self):
+        self.fail("Not yet implemented") 
