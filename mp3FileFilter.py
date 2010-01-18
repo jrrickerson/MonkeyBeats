85e31c7f4a3b6082523a4a12193ca008ac11c89f
Russell Myers <myers.rw@gmail.com>
2010-01-17 22:45:33 -0500

    Refactoring common filter logic into FileFilter class. Shortening some statements.

--- mp3FileFilter.py
+++ mp3FileFilter.py
@@ -11,26 +11,19 @@ def __nonmp3filter__(file):
     result = re.search("mp3$", file)
     return result is None
 
-class Mp3FileFilter:
-    def __init__(self, directory):
-        self.directory = directory
 
-    def __getUnfilteredFiles__(self):
-        return [os.path.join(root, file) for root, dirs, files in os.walk(self.directory) 
-                for file in files]
-
-    def getFiles(self):
-        allFiles = self.__getUnfilteredFiles__()
-        return filter(__mp3filter__, allFiles)
-
-class NonMp3FileFilter:
+class FileFilter:
     def __init__(self, directory):
         self.directory = directory
     
     def __getUnfilteredFiles__(self):
-        return [os.path.join(root, file) for root, dirs, files in os.walk(self.directory)
+        return [os.path.join(root, file) for root, dirs, files in os.walk(self.directory) 
                 for file in files]
 
+class Mp3FileFilter(FileFilter):
     def getFiles(self):
-        allFiles = self.__getUnfilteredFiles__()
-        return filter(__nonmp3filter__, allFiles)
+        return filter(__mp3filter__, self.__getUnfilteredFiles__())
+
+class NonMp3FileFilter(FileFilter):
+    def getFiles(self):
+        return filter(__nonmp3filter__, self.__getUnfilteredFiles__())
