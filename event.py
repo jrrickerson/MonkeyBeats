4cb6b0bc6e8b4e9795870623a7b3f906fe0f0e85
Russell Myers <myers.rw@gmail.com>
2010-01-19 20:09:38 -0500

    Adding event.py. This was duplicated from http://www.valuedlessons.com/2008/04/events-in-python.html

---
+++ event.py
@@ -0,0 +1,26 @@
+class Event:
+    def __init__(self):
+        self.handlers = set()
+
+    def handle(self, hanndler):
+        self.handlers.add(handler)
+        return self
+
+    def unhandle(self, handler):
+        try:
+            self.handlers.remove(handler)
+        except:
+            raise ValueError("Handler not handling this event. Cannot delete");
+        return self
+
+    def fire(self, *args, **kargs):
+        for handler in self.handlers:
+            handler(*args, **kargs)
+
+    def getHandlerCount(self):
+        return len(self.handlers)
+
+    __iadd__ = handle
+    __isub__ = unhandle
+    __call__ = fire
+    __len__ = getHandler
