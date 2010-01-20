e3574a7866ff32b3b1d7327f258709df56d4dd04
JR Rickerson <jrrickerson@gmail.com>
2010-01-19 22:20:12 -0500

    Add more unit tests for file filters

--- tests/test_fileFilters.py
+++ tests/test_fileFilters.py
@@ -23,6 +23,14 @@ class Mp3FileFilterTestFixture(unittest.TestCase):
         mp3Filter = Mp3FileFilter(fileSource)
         self._filteredList = mp3Filter.getFiles()
 
+    def generateLongFileNames(self):
+        pathComponents = ["root", "folderA", "folderB", "nestedFolder", "verylongfoldername",
+            "directory", "music", "oldmusic", "albums", "downloads"]
+        files = ["file{0}.mp3".format(num) for num in xrange(0, 100)]
+        fullPaths = [os.path.join(os.path.join(*pathComponents), fileName) for fileName in files]
+        return fullPaths
+        
+
     def testAllSourceFilesMatchFilter(self):
         self._initialFileList = ["happy.mp3", "sad.mp3", "fun.mp3", "goofy.mp3"]
         self._expectedFileList = self._initialFileList
@@ -31,6 +39,22 @@ class Mp3FileFilterTestFixture(unittest.TestCase):
     def testNoSourceFilesMatchFilter(self):
         self._initialFileList = ["happy.ogg", "sad.txt", "fun.wav", "goofy"]
         self._expectedFileList = []
+        self.applyFilter()
+
+    def testFilterMatchesOnlyMp3Extension(self):
+        self._initialFileList = ["happy.ogg", "file1.mp3", "sad.txt", "GreatMusic.mp3", "fun.wav", "goofy",
+            "falsePositive.mp3.bak", "mp3index.txt", "mp3/index.html"]
+        self._expectedFileList = ["file1.mp3", "GreatMusic.mp3"]
+        self.applyFilter()
+
+    def testEmptyFileList(self):
+        self._initialFileList = []
+        self._expectedFileList = []
+        self.applyFilter()
+
+    def testDeepPaths(self):
+        self._initialFileList = self.generateLongFileNames()
+        self._expectedFileList = self._initialFileList
         self.applyFilter()
 
 class MockFileSource:
