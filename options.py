c9d557824c22e0cb0a9ac419b5dc7312d69bc729
JR Rickerson <jrrickerson@gmail.com>
2010-01-22 00:00:20 -0500

    Added command line options parser

--- options.py
+++ options.py
@@ -1,5 +1,32 @@
-from parseopt import OptionParser
+from optparse import OptionParser
 
 class Options:
-    def __init__(self, argList):
+    def __init__(self):
+        self.startGuiMode = false
+        self.backupLocation = ""
+        self.followSymbolicLinks = false
+        self.rootDirectory = "."
+
+class OptionsLoader:
+    def loadFromArguments(self, argumentList):
+        options = Options()
+        self._intializeCommandLineParser()
+        (options, args) = self._parser.parse_args(argumentList)
+        return options
+
+    def loadFromConfigurationFile(self, file):
+        raise RuntimeException("Not yet implemented")
+
+    def _intializeCommandLineParser(self):
         self._parser = OptionParser()
+        self._parser.add_option("-g", "--gui", dest="startGuiMode",
+            help="Start in GUI mode", action="store_true")
+        self._parser.add_option("-f", "--followLinks", dest="followSymbolicLinks",
+            help="Follow symbolic links to target directories", action="store_true")
+        self._parser.add_option("-b", "--backup", dest="backupLocation",
+            metavar="BACKUP_DIR", help="Backup unrelated files to BACKUP_DIR")
+        self._parser.add_option("-d", "--directory", dest="rootDirectory",
+            metavar="ROOT_DIR", help="Search for files starting in ROOT_DIR [default %default]")
+        self._parser.set_defaults(startGuiMode=False, followSymbolicLinks=False, backupLocation="",
+            rootDirectory=".")
+
