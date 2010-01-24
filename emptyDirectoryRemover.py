6219bac9a76aeddb8859bf8a3d691475e2d9dff4
Russell Myers <myers.rw@gmail.com>
2010-01-24 17:35:53 -0500

    Adding critical import statements and using correct variable.

--- emptyDirectoryRemover.py
+++ emptyDirectoryRemover.py
@@ -1,10 +1,13 @@
+import os
+import shutil
+
 class EmptyDirectoryRemover:
     """ Removes the empty directories within the specified location."""
     def __init__(self, location):
         self.location = location
 
     def remove(self):
-        for path, dirs, files in os.walk(location):
+        for path, dirs, files in os.walk(self.location):
             if len(files) == 0:
                 shutil.rmtree(path, ignore_errors=True)
         
